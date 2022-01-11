f9ddf357ec022b63f4797e015af34fef4c0ed0bc
luongtx <tranxuanluong1998@gmail.com>
2022-01-11 21:56:27 +0700

    Update db info

--- src/main/java/com/luongtx/oes/repository/CatalogRepo.java
+++ src/main/java/com/luongtx/oes/repository/CatalogRepo.java
@@ -1,15 +1,13 @@
 package com.luongtx.oes.repository;
 
+import com.luongtx.oes.entity.Catalog;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import com.luongtx.oes.entity.Catalog;
 
 @Repository
 public interface CatalogRepo extends JpaRepository<Catalog, Long> {
